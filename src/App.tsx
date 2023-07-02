6e0a6efcd603293c96b0433f9b432b5bae71c1fe
SRK <learn_git.com>
2023-07-02 14:29:41 +0530

    Build a Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,31 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import "./App.css";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
