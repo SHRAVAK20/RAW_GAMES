aca9b1ba82d5deb40865fac3ee5ee2cf92e7e7a9
SRK <choudharyshravak@gmail.com>
2023-08-06 17:09:21 +0530

    commenting usestate in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ export interface GameQuery {
 }
 
 function App() {
-  const [gameQuery, setGameQuery] = useState({} as GameQuery);
+  // const [gameQuery, setGameQuery] = useState({} as GameQuery);
   return (
     <>
       <Grid
