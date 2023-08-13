2eb1dc91709fac384fc4edc27a9e1dd979175dc5
SRK <choudharyshravak@gmail.com>
2023-08-13 22:59:30 +0530

    fix the nav bar

--- src/compenents/SearchInput.tsx
+++ src/compenents/SearchInput.tsx
@@ -3,16 +3,19 @@ import { useRef } from "react";
 import styles from "./Nav.module.scss";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
   const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
   return (
     <form
       className={styles.searchBarContainer}
       onSubmit={(event) => {
         event.preventDefault();
         setSearchText(ref?.current?.value || "");
+        navigate("/");
       }}
     >
       <InputGroup>
